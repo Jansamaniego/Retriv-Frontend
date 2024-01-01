453d8a4cee05ff99f9f5cce4796cb5eeac9bf40c
Jan Samaniego <100932229+Jansamaniego@users.noreply.github.com>
2024-01-02 03:12:04 +0800

    set cookie on home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,11 +2,24 @@ import { PageFlexColumnWrapper } from 'components/common';
 import ProductManager from 'components/product/productManager';
 import CategoryManager from 'components/category/categoryManager';
 import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import Cookies from 'universal-cookie';
 
 export const Home = () => {
   const [searchParams] = useSearchParams();
 
-  console.log(searchParams.get('googleSignIn'));
+  useEffect(() => {
+    if (searchParams.get('googleSignIn')) {
+      const cookies = new Cookies(null, { path: '/' });
+      cookies.set('logged_in', true, {
+        maxAge: Number(process.env.REACT_APP_API_WEB_BASE_URL) * 60 * 1000,
+        httpOnly: false,
+        sameSite: 'none',
+        secure: true,
+      });
+    }
+  }, [searchParams]);
+
   return (
     <PageFlexColumnWrapper>
       <ProductManager isProductSearchControlsOpen={true} />
