49c8cfc806c503dbf8f93e97e3dd2dbc2a4220fb
Jan Samaniego <100932229+Jansamaniego@users.noreply.github.com>
2024-01-02 03:03:54 +0800

    searchParams on home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,12 @@
 import { PageFlexColumnWrapper } from 'components/common';
 import ProductManager from 'components/product/productManager';
 import CategoryManager from 'components/category/categoryManager';
+import { useSearchParams } from 'react-router-dom';
 
 export const Home = () => {
+  const [searchParams] = useSearchParams();
+
+  console.log(searchParams.get('googleSignIn'));
   return (
     <PageFlexColumnWrapper>
       <ProductManager isProductSearchControlsOpen={true} />
